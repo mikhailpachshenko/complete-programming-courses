47d81473cbae847f8d9dc14337ce81838677e50f
mikhailpachshenko <mikhailpachshenko@gmail.com>
2022-12-14 21:05:09 +0600

    add new some practic to 28 block

---
+++ skillbox - Профессия Go-разработчик/28's block/28.5 Folders (structer)/28.5_10/cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	ss "module/pkg/repo/students_repo"
+	s "module/pkg/student"
+)
+
+func main() {
+	storage := ss.NewStudentsStorage()
+	for {
+		new := s.NewStudent()
+		fmt.Println("Введите имя, возраст и оценку студента:")
+		_, err := fmt.Scan(&new.Name, &new.Age, &new.Grade)
+		if err == io.EOF {
+			break
+		}
+		if err = storage.Put(new); err != nil {
+			fmt.Println("error:", err.Error())
+			return
+		}
+	}
+	fmt.Println("Хранилище студентов:")
+	for _, v := range storage.Get() {
+		fmt.Println(v.Name, v.Age, v.Grade)
+	}
+}
